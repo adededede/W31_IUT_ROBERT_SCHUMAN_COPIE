07f13c2db7181ac6c8649c2b655c1a53c89ab370
adededede <80714680+adededede@users.noreply.github.com>
2022-11-25 20:15:50 +0100

    VERSION PROF

---
+++ correction/TP6/routes/web.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::group( [], function() {
+
+	session_start();
+
+	/* ------------------------------------------------------------------------
+	| Public routing */
+	Route::get('/', function () { return view('signin',['message' => $_SESSION['message']??null]); });
+	Route::post('adduser', function () { return view('adduser'); });
+	Route::post('authenticate', function () { return view('authenticate'); });
+	Route::get('signin', function () { return view('signin',['message' => $_SESSION['message']??null]); });
+	Route::get('signup', function () { return view('signup',['message' => $_SESSION['message']??null]); });
+	// ------------------------------------------------------------------------
+
+	/* ------------------------------------------------------------------------
+	| Admin routing */
+	Route::prefix('admin')->group( function() {
+
+		if ( !isset($_SESSION['user']) )
+			return redirect('signin');
+
+		Route::post('changepassword', function () { return view('changepassword'); });
+		Route::get('deleteuser', function () { return view('deleteuser'); });
+		Route::get('formpassword', function () { return view('formpassword',['message' => $_SESSION['message']??null]); });
+		Route::get('account', function () {
+			return view('account', ['user' => $_SESSION['user'], 'message' => $_SESSION['message']??null]);
+		});
+		Route::get('signout', function () {
+			session_destroy();
+			return redirect('signin');
+		});
+
+	});
+	// ------------------------------------------------------------------------
+
+});
